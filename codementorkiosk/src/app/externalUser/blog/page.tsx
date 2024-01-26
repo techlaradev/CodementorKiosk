5893329a2dc4e2ac05b955b62fb2c1d1e37a74d8
Lara Ayne <lara.cardoso@pullse.online>
2024-01-26 12:10:57 -0300

    feat: add code playground and blog updates

--- codementorkiosk/src/app/externalUser/blog/page.tsx
+++ codementorkiosk/src/app/externalUser/blog/page.tsx
@@ -1,4 +1,5 @@
-import { BlogKioske } from "@/components/component/blog-kiosk"
+import BlogKioske from "@/components/component/blog-kiosk"
+
 export default function blogKiosk(){
 
     return <BlogKioske/>
---
+++ codementorkiosk/src/app/internalUser/code-playground/page.tsx
@@ -0,0 +1,5 @@
+import CodeWave from "@/components/component/codeplayground"
+export default function codeWavecp(){
+
+    return <CodeWave/>
+}
